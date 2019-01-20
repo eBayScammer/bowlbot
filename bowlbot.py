2b73cbcdc40fe201d83a3bb009a941dd0ddcddab
eBayScammer <46870270+eBayScammer@users.noreply.github.com>
2019-01-20 12:56:06 -0500

    Update bowlbot.py

--- bowlbot.py
+++ bowlbot.py
@@ -19,5 +19,8 @@ async def on_message(message):
     if message.author.id != client.user.id and "bowl" in message.content:
         await client.send_message(message.channel, message.content.replace("bowl", "<:bowlcut:492887126820651028>"))
         await client.delete_message(message)
+    if message.author.id != client.user.id and "max" in message.content:
+        await client.send_message(message.channel, message.content.replace("max", "<:max:484169938156912641>"))
+        await client.delete_message(message)
 
 client.run(os.getenv('TOKEN'))
