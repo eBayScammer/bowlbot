834fc0bce74d0d68f138fcccd4711a0b6b537b27
eBayScammer <46870270+eBayScammer@users.noreply.github.com>
2019-01-20 12:46:18 -0500

    Update bowlbot.py

--- bowlbot.py
+++ bowlbot.py
@@ -17,7 +17,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author.id != client.user.id and "bowl" in message.content:
-        await client.send_message(message.channel, message.content.replace("bowl", "<:a:536600885489434645>"))
+        await client.send_message(message.channel, message.content.replace("bowl", "<:bowlcut:492887126820651028>"))
         await client.delete_message(message)
 
 @client.event
@@ -25,7 +25,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == 'i got the bot running 24/7 now wonder if its working':
-        role = get(message.server.roles, name='Admin')
+        role = get(message.server.roles, name='Bot')
         await client.add_roles(message.author, role)
 
 
