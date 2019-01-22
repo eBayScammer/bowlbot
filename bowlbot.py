a61752755551b1ceda748d7f6bbadb6b74682f08
eBayScammer <46870270+eBayScammer@users.noreply.github.com>
2019-01-21 21:56:09 -0500

    Update bowlbot.py

--- bowlbot.py
+++ bowlbot.py
@@ -20,12 +20,10 @@ async def on_message(message):
     if message.author.id != client.user.id and "bowl" in message.content:
         await client.send_message(message.channel, message.content.replace("bowl", "<:bowlcut:492887126820651028>"))
         await client.delete_message(message)
-    if message.author.id != client.user.id and "max" in message.content:
+    elif message.author.id != client.user.id and "max" in message.content:
         await client.send_message(message.channel, message.content.replace("max", "<:max:536609206829056014>"))
         await client.delete_message(message)
-    if message.author == client.user:
-        return
-    elif message.content.startswith("!8bowl"):
+    elif message.content.startswith("!8"):
         await client.send_message(message.channel, random.choice(["It is certain",
                                                                   "It is decidedly so",
                                                                   "Very doubtful",
